2bf9ee1098d455be5579dda0ce5602d021dca463
ThatDudeKondd <ThatDudeKondd@protonmail.com>
2026-07-31 07:14:38 +0200

    Started working on sync FINALY

---
+++ src/commands/utility/sync.ts
@@ -0,0 +1,7 @@
+import { defineCommand } from "../../utils/defineCommand.js";
+export default defineCommand({
+  name: "sync",
+  description: "Syncronise the Database with current users and guild.",
+  cooldown: 3000,
+  execute: async (ctx) => {},
+});
